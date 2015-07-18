f15870cee345f19254a154ac76c88b90ed20c7b1
Oystein Olsen <mail@oysteinolsen.com>
2015-07-18 22:02:54 +0200

    Read JSON database for locations

--- Source/App.js
+++ Source/App.js
@@ -7,69 +7,39 @@ var viewer = new Cesium.Viewer('cesiumContainer', {
 	    })
     });
 
-var pos1 = {
-    destination : Cesium.Cartesian3.fromDegrees(10.41651047, 63.43911310, 1000),
-    orientation : {
-	heading : 0.0,
-	pitch : Cesium.Math.PI_OVER_FIVE,
-	roll : 0.0
+function set_destination (location_data) {
+    var pos = {
+        destination : Cesium.Cartesian3.fromDegrees(
+                              location_data.longitude,
+                              location_data.latitude,
+                              location_data.height),
+        orientation : {
+        heading : Cesium.Math.toRadians(location_data.heading),
+        pitch : location_data.pitch,
+        roll : location_data.roll
+        }
     }
+    return pos
 }
 
-var pos2 = {
-    destination : Cesium.Cartesian3.fromDegrees(10.41651047, 63.43911310, 500),
-    orientation : {
-	heading : 0.0,
-	pitch : 0.0,
-	roll : Cesium.Math.PI_OVER_FOUR
-    }
-}
-var posA = {
-    destination : Cesium.Cartesian3.fromDegrees(10.788634, 59.876789, 1000),
-    orientation : {
-	heading : 0.0,
-	pitch : 0.0,
-	roll : 0.0
-    }
-}
-var posB = {
-    destination : Cesium.Cartesian3.fromDegrees(10.789788, 59.968241, 1000),
-    orientation : {
-	heading : Cesium.Math.toRadians(315),
-	pitch : 0.0,
-	roll : 0.0
-    }
-}
-var posC = {
-    destination : Cesium.Cartesian3.fromDegrees(8.391132, 61.170962, 1000),
-    orientation : {
-	heading : Cesium.Math.toRadians(45),
-	pitch : 0.0,
-	roll : 0.0
-    }
-}
-var posD = {
-    destination : Cesium.Cartesian3.fromDegrees(8.371959, 61.202269, 1000),
-    orientation : {
-	heading : Cesium.Math.toRadians(180),
-	pitch : 0.0,
-	roll : 0.0
-    }
+function reqListener () {
+    locations = JSON.parse(this.responseText);
 }
 
-var pos_list = [posA, posB, posC, posD]
+var oReq = new XMLHttpRequest();
+oReq.onload = reqListener;
+oReq.open("get", "db.json", true);
+oReq.send();
+
 var i = 0
 document.addEventListener('keydown', function(e) {
     switch(e.keyCode){
-    case 'A'.charCodeAt(0):
-	viewer.camera.flyTo(pos1);
-	break;
-    case 'B'.charCodeAt(0):
-	viewer.camera.flyTo(pos2);
-	break;
-    case 'N'.charCodeAt(0):
-	viewer.camera.flyTo(pos_list[i]);
-	i++;
-	break;
+        case 'N'.charCodeAt(0):
+            viewer.camera.flyTo(set_destination(locations[i]));
+            i++;
+            if (i >= locations.length) {
+                i = 0;
+            }
+        break;
     }
 }, false);
